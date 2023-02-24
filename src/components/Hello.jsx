6b5cba248d8110433214945f5fe9daacbbfc0223
fhabhishektiwari <abhishektiwarifh@gmail.com>
2023-02-24 23:00:42 +0530

    added good morning

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -4,7 +4,7 @@ const Greet = (props) => {
     console.log(props);
   return (
     <div>
-      <h1>Hello {props.name} a.k.a {props.heroName}</h1>
+      <h1>Hello {props.name}, Good morning, a.k.a {props.heroName}</h1>
       {props.children}
     </div>
   )
