046001643fa9525511547d12cfd61de31e5d01fd
fhabhishektiwari <abhishektiwarifh@gmail.com>
2023-02-27 23:08:03 +0530

    Added new data

--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -7,6 +7,7 @@ const persons = [
     { id: 3, name: 'Amit', age: 23, skills: 'vue js' },
     { id: 4, name: 'Anirudh', age: 27, skills: 'node js' },
     { id: 5, name: 'Jaish', age: 24, skills: 'angular js' },
+    { id: 6, name: 'Mohit', age: 24, skills: 'javaScript' },
 
 ];
 const NameList = () => {
